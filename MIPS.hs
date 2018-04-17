642093590470357d51cf983a75ed11e85217e900
utashih <utashih@gmail.com>
2018-04-17 20:38:46 +0800

    add signature to register constants

--- MIPS.hs
+++ MIPS.hs
@@ -4,11 +4,15 @@ module MIPS where
 
 import Control.Monad (ap, liftM)
 import Data.Word (Word32)
+import Register (Register, zero, at, v0, v1, 
+    a0, a1, a2, a3, t0, t1, t2, t3, t4, t5, t6, t7, s0, s1, 
+    s2, s3, s4, s5, s6, s7, t8, t9, k0, k1, gp, sp, fp, ra)
 
 type Address = Word32
 type Bytecode = Word32
+type Program = [Bytecode]
 
-newtype MIPS a = MIPS { runMIPS :: Address -> ([Bytecode], Address, a) }
+newtype MIPS a = MIPS { runMIPS :: Address -> (Program, Address, a) }
     
 instance Functor MIPS where 
     fmap = liftM
@@ -30,6 +34,6 @@ word w = MIPS $ \addr -> ([w], addr + 1, ())
 label :: MIPS Address 
 label = MIPS $ \addr -> ([], addr, addr)
 
-assemble :: MIPS a -> [Bytecode]
+assemble :: MIPS a -> Program
 assemble m = bytecodes 
     where (bytecodes, _, _) = runMIPS m 0x00000000
--- Register.hs
+++ Register.hs
@@ -50,6 +50,10 @@ register ('$':name) = do
             ret idx
 register _ = Nothing
 
+zero, at, v0, v1, a0, a1, a2, a3 :: Register
+t0, t1, t2, t3, t4, t5, t6, t7 :: Register
+s0, s1, s2, s3, s4, s5, s6, s7 :: Register
+t8, t9, k0, k1, gp, sp, fp, ra :: Register
 zero = Register 0
 at   = Register 1
 v0   = Register 2
