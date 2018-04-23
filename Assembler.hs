714a77fcce54d0991b4cf8ea80add2c716db5885
utashih <utashih@gmail.com>
2018-04-23 11:32:36 +0800

    add data definition

--- Assembler.hs
+++ Assembler.hs
@@ -1,3 +1,30 @@
 module Assembler where 
 
 import Parser (ASTLine(..), parseASM)
+import Register (Register(..), register)
+
+data Instruction
+    = RIns Opcode Register Register Register Shamt Funct
+    | IIns Opcode Register Register Integer
+    | JIns Opcode Integer
+    deriving (Show, Eq)
+
+type Opcode = Integer 
+type Shamt = Integer
+type Funct = Integer
+
+rIns :: Integer -> String -> String -> String -> Integer -> Integer -> Either String Instruction
+rIns opcode _ _ _ _ _ | opcode < 0|| opcode >= 2^6 = Left $ show opcode ++ "cannot be opcode"
+rIns _ _ _ _ shamt _ | shamt < 0 || shamt >= 2^5 = Left $ show shamt ++ "cannot be shamt"
+rIns _ _ _ _ _ funct | funct < 0 || funct >= 2^6 = Left $ show funct ++ "cannot be funct"
+rIns opcode srs srt srd shamt funct = 
+    let mrs = do  
+        rs <- register srs 
+        rt <- register srt
+        rd <- register srd 
+        return (rs, rt, rd)
+    in case mrs of 
+        Nothing           -> Left "Wrong register"
+        Just (rs, rt, rd) -> Right $ RIns opcode rs rt rd shamt funct 
+
+        
