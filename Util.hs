1a0cbe0ec596d7beeec5391dc2e39b724623f553
utashih <utashih@gmail.com>
2018-04-26 22:07:41 +0800

    add immediate sign-ext

--- Util.hs
+++ Util.hs
@@ -11,3 +11,10 @@ wordHex = printf "%08x"
 
 partialSum :: Num a => [a] -> [a]
 partialSum = scanl (+) 0
+
+immSext :: Int -> Integer -> Integer 
+immSext w imm = 
+    if imm >= bound
+        then imm - bound * 2 
+        else imm 
+    where bound = 2^(w-1)
